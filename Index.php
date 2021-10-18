1301c70df68ccb084548ac3d13f3b811215770d7
Nyanns <satrianindhita6@gmail.com>
2021-10-19 01:22:43 +0700

    mengedit file index.php

--- Index.php
+++ Index.php
@@ -2,3 +2,6 @@
 echo "<h2> Selamat berlatih menjadi kolaborator </h2>";
 echo "<h3>Isi Nama dan Nim Anda di bawah baris code ini</h3>";
 echo "<h3> Kharisma satria nindhita (205410129)</h3>"
+======
+>>>>>> upstream/master
+?>
