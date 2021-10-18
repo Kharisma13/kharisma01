0ff44d05936c07b4de1892f2cd2e175e39ab48c0
Nyanns <satrianindhita6@gmail.com>
2021-10-19 01:49:34 +0700

    satuuu

--- Index.php
+++ Index.php
@@ -1,6 +1,7 @@
 <?php
 echo "<h2> Selamat berlatih menjadi kolaborator </h2>";
 echo "<h3>Isi Nama dan Nim Anda di bawah baris code ini</h3>";
+<<<<<< HEAD
 echo "<h3> Kharisma satria nindhita (205410129)</h3>"
 ======
 >>>>>> upstream/master
